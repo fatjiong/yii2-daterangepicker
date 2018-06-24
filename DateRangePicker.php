6b9916d7e279cb58f6832301a85c61f6ea699119
fatjiong <757970599@qq.com>
2018-06-25 00:23:55 +0800

    [*] 修改调用模式

--- DateRangePicker.php
+++ DateRangePicker.php
@@ -1,60 +1,63 @@
 <?php
 namespace fatjiong\daterangepicker;
+
 // 胖纸囧
-use Yii;
-use yii\widgets\InputWidget;
-use yii\web\View;
 use yii\helpers\Html;
 use yii\helpers\Json;
+use yii\web\View;
+use yii\widgets\InputWidget;
 
 class DateRangePicker extends InputWidget
 {
-	public $selector;
-	public $callback;
-	public $options = [];
-	public $htmlOptions = [];
-	public $moment = true;
+    public $selector;
+    public $callback;
+    public $options     = [];
+    public $htmlOptions = [];
+    public $moment      = true;
 
-	public function init()
-	{
-		if (!isset($this->htmlOptions['id'])) {
-			$this->htmlOptions['id'] = $this->getId();
-		}
-		if (!isset($this->htmlOptions['name'])) {
-			$this->htmlOptions['name'] = Html::getInputName($this->model, $this->attribute);
-		}
-		parent::init();
-	}
+    public function init()
+    {
+        if (!isset($this->htmlOptions['id'])) {
+            $this->htmlOptions['id'] = $this->getId();
+        }
+        if (!$this->name) {
+            $this->htmlOptions['name'] = $this->hasModel() ? Html::getInputName($this->model, $this->attribute) : $this->id;
+            $this->name                = $this->htmlOptions['name'];
+        } else {
+            $this->htmlOptions['name'] = $this->name;
+        }
 
-	public function run()
-	{
-		$this->registerPlugin();
-	}
+        parent::init();
+    }
 
-	protected function registerPlugin()
-	{
+    public function run()
+    {
+        $this->registerPlugin();
+    }
 
-		if ($this->moment) {
-			DateRangePickerAsset::$extra_js[] = defined('YII_DEBUG') && YII_DEBUG ? 'moment.js' : 'moment.min.js';
-		}
+    protected function registerPlugin()
+    {
 
-		if ($this->selector)
-		{
-			$this->registerJs($this->selector, $this->options, $this->callback);
-		} else {
-			$id = $this->htmlOptions['id'];
-			echo Html::tag('input', '', $this->htmlOptions);
-			$this->registerJs("#{$id}", $this->options, $this->callback);
-		}
-	}
+        if ($this->moment) {
+            DateRangePickerAsset::$extra_js[] = defined('YII_DEBUG') && YII_DEBUG ? 'moment.js' : 'moment.min.js';
+        }
 
-	protected function registerJs($selector, $options, $callback) {
-		$view = $this->getView();
-		DateRangePickerAsset::register($view);
-		$js   = [];
-		$js[] = '$("' . $selector . '").daterangepicker(' . Json::encode($options) . ($callback ? ', ' . Json::encode($callback) : '') . ');';
-		$view->registerJs(implode("\n", $js),View::POS_READY);
+        if ($this->selector) {
+            $this->registerJs($this->selector, $this->options, $this->callback);
+        } else {
+            $id = $this->htmlOptions['id'];
+            echo Html::tag('input', '', $this->htmlOptions);
+            $this->registerJs("#{$id}", $this->options, $this->callback);
+        }
+    }
 
-	}
+    protected function registerJs($selector, $options, $callback)
+    {
+        $view = $this->getView();
+        DateRangePickerAsset::register($view);
+        $js   = [];
+        $js[] = '$("' . $selector . '").daterangepicker(' . Json::encode($options) . ($callback ? ', ' . Json::encode($callback) : '') . ');';
+        $view->registerJs(implode("\n", $js), View::POS_READY);
+
+    }
 }
-
